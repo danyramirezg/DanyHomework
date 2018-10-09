ff1ceafde9eb8ca559d9fdde3506b1581a3fde8a
Dany <danyramirezg@gmail.com>
2018-10-09 16:23:35 -0700

    Convert from Farenheit to Celsius

--- src/DanyHomework.java
+++ src/DanyHomework.java
@@ -51,3 +51,5 @@ calculate the number of letters.*/
     /*Define an integer variable called bankBalance.
     Initialize it to a value of 500. Then add 250
 to it. Then subtract 100 from it. Finally, print the resulting value.*/
+
+
---
+++ src/FarenheitExcercise.java
@@ -0,0 +1,8 @@
+public class FarenheitExcercise {
+    public static void main(String[]args){
+        double fahrenheit = 70;
+        double celsius = ((fahrenheit-32)*5)/9;
+        System.out.println(celsius);
+
+    }
+}
