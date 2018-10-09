7c843f47bfcf641007b05224bd3f06159bbda17e
Dany <danyramirezg@gmail.com>
2018-10-09 16:47:59 -0700

    Boolean= Exercise for statements trues or falses

---
+++ src/BooleanExcersices.java
@@ -0,0 +1,8 @@
+public class BooleanExcersices {
+    public static void main(String[]args){
+boolean isSlavaHappy = true;
+        if(isSlavaHappy){
+            System.out.println("He is super Happy");
+        }
+    }
+}
--- src/FarenheitExcercise.java
+++ src/FarenheitExcercise.java
@@ -3,6 +3,11 @@ public class FarenheitExcercise {
         double fahrenheit = 70;
         double celsius = ((fahrenheit-32)*5)/9;
         System.out.println(celsius);
+        double celsius1 = 22;
+        double fahrenheit1 = ((celsius1*9/5)+32);
+        System.out.println(fahrenheit1);
+
+/*I would like to do something like a question, write celsius or fahrenheit*/
 
     }
 }
